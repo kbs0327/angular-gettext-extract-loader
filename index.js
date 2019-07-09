26609307f655c78080a58c4b3eafbf3d34cb748f
김부승/Dooray개발실/NE <buseung.kim@nhnent.com>
2019-07-09 12:33:00 +0900

    #dooray-공통/1316 loader에서는 일단 쌓고 추후에 계산하는 방식으로 변경

--- index.js
+++ index.js
@@ -5,175 +5,180 @@ const PO = require('pofile');
 const _ = require('lodash');
 
 const {
-    OriginalSource,
+  OriginalSource,
 } = require("webpack-sources");
 
 function matches (oldRef, newRef) {
-    return _(oldRef).split(':').first() === _(newRef).split(':').first();
+  return _(oldRef).split(':').first() === _(newRef).split(':').first();
 }
 
 function mergeReferences (oldRefs, newRefs) {
-    const _newRefs = _(newRefs);
+  const _newRefs = _(newRefs);
 
-    return _(oldRefs)
-        .reject(function (oldRef) {
-            return _newRefs.any(function (newRef) {
-                return matches(oldRef, newRef);
-            });
-        })
-        .concat(newRefs)
-        .uniq()
-        .sort()
-        .value();
+  return _(oldRefs)
+      .reject(function (oldRef) {
+        return _newRefs.any(function (newRef) {
+          return matches(oldRef, newRef);
+        });
+      })
+      .concat(newRefs)
+      .uniq()
+      .sort()
+      .value();
 }
 
 function castingAsPoItem(item) {
-    return _.reduce(item, (result, value, key) => {
-        if (value instanceof PO.Item) {
-            return result;
-        }
-        result[key] = new PO.Item();
-        _.assign(result[key], value);
-        return result;
-    }, item);
+  return _.reduce(item, (result, value, key) => {
+    if (value instanceof PO.Item) {
+      return result;
+    }
+    result[key] = new PO.Item();
+    _.assign(result[key], value);
+    return result;
+  }, item);
 }
 
 class AngularGettextPlugin {
-    constructor(options) {
-        this.options = _.cloneDeep(options);
-        this.options.postProcess = this.options.postProcess || function () {};
-        this.options.langList = this.options.langList || [];
-        this.strings = {};
-        this.oldStrings = {};
-    }
-    apply(compiler) {
-        const self = this;
-        const NO_CONTEXT = '$$noContext';
-        const compilerOptions = this.options;
-        let firstRun = true;
+  constructor(options) {
+    this.options = _.cloneDeep(options);
+    this.options.postProcess = this.options.postProcess || function () {};
+    this.options.langList = this.options.langList || [];
+    this.strings = {};
+    this.oldStrings = {};
+    this.datas = [];
+  }
+  apply(compiler) {
+    const self = this;
+    const NO_CONTEXT = '$$noContext';
+    const compilerOptions = this.options;
+    let firstRun = true;
 
-        function collectData(request, data) {
+    const createDummyFiles = (compiler, cb) => {
+      if (!firstRun) {
+        return cb();
+      }
+      firstRun = false;
+      mkdirSyncRecursive(compilerOptions.baseDir);
+      const filePathList = _.flatten(_.map(compilerOptions.langList, locale => {
+        return _.map(compilerOptions.resultFiles, metadata => {
+          let filename = _.isFunction(metadata.filename) ? metadata.filename(locale) : metadata.filename;
+          return path.resolve(compilerOptions.baseDir, filename);
+        });
+      }));
+      Promise.all(_.map(filePathList, filePath => {
+        return new Promise((resolve, reject) => {
+          if (!fs.existsSync(filePath)) {
+            return fs.writeFile(filePath, _.endsWith(filePath, '.json') ? '{}' : '', makePromiseCallback(resolve, reject));
+          }
+          resolve();
+        });
+      })).finally(cb);
+    };
+
+    compiler.hooks.run.tapAsync('AngularGettextPlugin', createDummyFiles);
+    compiler.hooks.watchRun.tapAsync('AngularGettextPlugin', createDummyFiles);
+
+    compiler.hooks.thisCompilation.tap('AngularGettextPlugin', compilation => {
+      compilation.hooks.normalModuleLoader.tap('AngularGettextPlugin', loaderContext => {
+        loaderContext.emitData = (request, data) => {
+          this.datas.push(data);
+        };
+      });
+
+      compilation.hooks.finishModules.tapAsync('AngularGettextPlugin', (modules, cb) => {
+        if (this.datas.length > 0) {
+          _.forEach(this.datas, data => {
             _.forEach(data, (value, key) => {
-                value = castingAsPoItem(value);
-                let existing = self.strings[key];
-                if (!existing) {
-                    self.strings[key] = value;
-                    return;
-                }
+              value = castingAsPoItem(value);
+              let existing = this.strings[key];
+              if (!existing) {
+                this.strings[key] = value;
+                return;
+              }
 
-                const item = value[NO_CONTEXT];
-                existing = existing[NO_CONTEXT];
-                if (item && existing) {
-                    existing.comments = _.uniq(existing.comments.concat(item.comments)).sort();
-                    existing.references = mergeReferences(item.references, existing.references);
-                    return;
-                }
-                self.strings[key] = _.assign(self.strings[key], value)
+              const item = value[NO_CONTEXT];
+              existing = existing[NO_CONTEXT];
+              if (item && existing) {
+                existing.comments = _.uniq(existing.comments.concat(item.comments)).sort();
+                existing.references = mergeReferences(item.references, existing.references);
+                return;
+              }
+              this.strings[key] = _.assign(this.strings[key], value)
             });
+          });
+          this.datas.length = 0;
+        }
+        this.hasExtraKeys = _.some(this.strings, (value, key) => {
+          const oldValue = this.oldStrings[key];
+          return !oldValue || !_.every(_.keys(value), context => oldValue[context]);
+        })
+        if (!this.hasExtraKeys) {
+          return cb();
+        }
+        this.oldStrings = this.strings;
+
+        if (compilerOptions.pofile) {
+          Promise.all(_.map(compilerOptions.langList, lang => new Promise(resolve => {
+            fs.writeFile(compilerOptions.pofile, Extractor.prototype.toString.call(this, lang), resolve);
+          }))).then(cb);
         }
 
-        const createDummyFiles = (compiler, cb) => {
-            if (!firstRun) {
-                return cb();
-            }
-            firstRun = false;
-            mkdirSyncRecursive(compilerOptions.baseDir);
-            const filePathList = _.flatten(_.map(compilerOptions.langList, locale => {
-                return _.map(compilerOptions.resultFiles, metadata => {
-                    let filename = _.isFunction(metadata.filename) ? metadata.filename(locale) : metadata.filename;
-                    return path.resolve(compilerOptions.baseDir, filename);
-                });
-            }));
-            Promise.all(_.map(filePathList, filePath => {
-                return new Promise((resolve, reject) => {
-                    if (!fs.existsSync(filePath)) {
-                        return fs.writeFile(filePath, _.endsWith(filePath, '.json') ? '{}' : '', makePromiseCallback(resolve, reject));
-                    }
-                    resolve();
-                });
-            })).finally(cb);
-        };
+        if (compilerOptions.saveCallback) {
+          compilerOptions.saveCallback(Extractor.prototype.toPo.call(this), makeSaveCallbackCb(compilation, cb));
+        }
+      });
+    });
 
-        compiler.hooks.run.tapAsync('AngularGettextPlugin', createDummyFiles);
-        compiler.hooks.watchRun.tapAsync('AngularGettextPlugin', createDummyFiles);
-
-        compiler.hooks.thisCompilation.tap('AngularGettextPlugin', compilation => {
-            compilation.hooks.normalModuleLoader.tap('AngularGettextPlugin', loaderContext => {
-                loaderContext.emitData = collectData
-            });
-
-            compilation.hooks.finishModules.tapAsync('AngularGettextPlugin', (modules, cb) => {
-                this.hasExtraKeys = _.some(this.strings, (value, key) => {
-                    const oldValue = this.oldStrings[key];
-                    return !oldValue || !_.every(_.keys(value), context => oldValue[context]);
-                })
-                if (!this.hasExtraKeys) {
-                    return cb();
-                }
-                this.oldStrings = this.strings;
-
-                if (compilerOptions.pofile) {
-                    Promise.all(_.map(compilerOptions.langList, lang => new Promise(resolve => {
-                        fs.writeFile(compilerOptions.pofile, Extractor.prototype.toString.call(this, lang), resolve);
-                    }))).then(cb);
-                }
-
-                if (compilerOptions.saveCallback) {
-                    compilerOptions.saveCallback(Extractor.prototype.toPo.call(this), makeSaveCallbackCb(compilation, cb));
-                }
-            });
-        });
-
-    }
+  }
 }
 
 function makeSaveCallbackCb(compilation, cb) {
-    return (
-        langList, poDatas, {baseDir, resultFiles} = {}
-    ) => {
-        if (!poDatas) {
-            return cb();
-        }
-        const promiseList = _.map(langList, locale => {
-            const localeList = [{
-                locale,
-                strings: Compiler.parsePoItems(poDatas[locale].items)
-            }];
-            return Promise.all(_.map(resultFiles, metadata => {
-                let data = new Compiler(metadata.compilerOptions).format(localeList);
-                data = metadata.postTransform ? metadata.postTransform(data) : data;
-                const filename = _.isFunction(metadata.filename) ? metadata.filename(locale) : metadata.filename;
-                const filepath = path.resolve(baseDir, filename);
-                const module = compilation.findModule(filepath);
-                return new Promise((resolve) => {
-                    fs.writeFile(filepath, data, 'utf8', err => {
-                        if (module) {
-                            if (_.endsWith(module.request, '.json')) {
-                                module.buildInfo.jsonData = JSON.parse(data);
-                            }
-                            module._source = new OriginalSource(data, filepath);
-                            module._cachedSources.clear();
-                            module.buildTimestamp = Date.now();
-                        }
-                        resolve();
-                    });
-                });
-            }));
-        });
-        Promise.all(promiseList).finally(cb);
+  return (
+      langList, poDatas, {baseDir, resultFiles} = {}
+  ) => {
+    if (!poDatas) {
+      return cb();
     }
+    const promiseList = _.map(langList, locale => {
+      const localeList = [{
+        locale,
+        strings: Compiler.parsePoItems(poDatas[locale].items)
+      }];
+      return Promise.all(_.map(resultFiles, metadata => {
+        let data = new Compiler(metadata.compilerOptions).format(localeList);
+        data = metadata.postTransform ? metadata.postTransform(data) : data;
+        const filename = _.isFunction(metadata.filename) ? metadata.filename(locale) : metadata.filename;
+        const filepath = path.resolve(baseDir, filename);
+        const module = compilation.findModule(filepath);
+        return new Promise((resolve) => {
+          fs.writeFile(filepath, data, 'utf8', err => {
+            if (module) {
+              if (_.endsWith(module.request, '.json')) {
+                module.buildInfo.jsonData = JSON.parse(data);
+              }
+              module._source = new OriginalSource(data, filepath);
+              module._cachedSources.clear();
+              module.buildTimestamp = Date.now();
+            }
+            resolve();
+          });
+        });
+      }));
+    });
+    Promise.all(promiseList).finally(cb);
+  }
 }
 
 function makePromiseCallback(resolve, reject) {
-    return err => err ? reject(err) : resolve();
+  return err => err ? reject(err) : resolve();
 }
 
 function mkdirSyncRecursive(directory) {
-    var path = directory.replace(/\/$/, '').split('/');
-    for (var i = 1; i <= path.length; i++) {
-        var segment = path.slice(0, i).join('/');
-        segment.length > 0 && !fs.existsSync(segment) ? fs.mkdirSync(segment) : null;
-    }
+  var path = directory.replace(/\/$/, '').split('/');
+  for (var i = 1; i <= path.length; i++) {
+    var segment = path.slice(0, i).join('/');
+    segment.length > 0 && !fs.existsSync(segment) ? fs.mkdirSync(segment) : null;
+  }
 };
 
 AngularGettextPlugin.loader = require.resolve('./loader');
